4c985ab9ac8224a7ba464e0f0343742c94027b66
Dawid Cielicki <gamer@o2.pl>
2018-12-17 17:40:51 +0100

    [add] Skeleton for adding and displaying product added. Some crazy stuff.

--- src/main/java/com/shopproject/shopdigger/model/enums/Unit.java
+++ src/main/java/com/shopproject/shopdigger/model/enums/Unit.java
@@ -1,4 +1,7 @@
 package com.shopproject.shopdigger.model.enums;
 
 public enum Unit {
+
+    G, KG, ML, L, PCS
+
 }
--- src/main/java/com/shopproject/shopdigger/service/impl/ProductServiceImpl.java
+++ src/main/java/com/shopproject/shopdigger/service/impl/ProductServiceImpl.java
@@ -1,4 +1,31 @@
 package com.shopproject.shopdigger.service.impl;
 
-public class ProductServiceImpl {
+import com.shopproject.shopdigger.dao.ProductRepository;
+import com.shopproject.shopdigger.model.Product;
+import com.shopproject.shopdigger.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Optional<Product> getProductById(Long productId){
+        return productRepository.findById(productId);
+    }
+
+    @Override
+    public boolean saveProduct(Product product) {
+        productRepository.save(product);
+        return true;
+    }
 }
